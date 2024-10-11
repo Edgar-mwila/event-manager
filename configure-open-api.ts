af2d21a51496879a69adbbf4fd7546373f7a1545
Edgar Mwila <edgarmwila84@gmail.com>
2024-10-11 18:14:17 +0200

    switching to other app

---
+++ configure-open-api.ts
@@ -0,0 +1,30 @@
+import { apiReference } from "@scalar/hono-api-reference";
+import packageJSON from "./package.json" with { type: "json" };
+import type { OpenAPIHono } from "@hono/zod-openapi";
+
+type AppOpenAPI = OpenAPIHono
+
+export default function configureOpenAPI(app: AppOpenAPI) {
+  app.doc("/doc", {
+    openapi: "3.0.0",
+    info: {
+      version: packageJSON.version,
+      title: "Tasks API",
+    },
+  });
+
+  app.get(
+    "/reference",
+    apiReference({
+      theme: "kepler",
+      layout: "classic",
+      defaultHttpClient: {
+        targetKey: "javascript",
+        clientKey: "fetch",
+      },
+      spec: {
+        url: "/doc",
+      },
+    }),
+  );
+}
--- server/app.ts
+++ server/app.ts
@@ -1,13 +1,32 @@
 import { Hono } from 'hono'
-import { logger } from 'hono/logger'
+import { pinoLogger } from './middlewares/logger'
 import { authRoutes } from './routes/auth';
 import eventsRoutes from './routes/events';
 import { ticketRoutes } from './routes/tickets';
 import { invitationRoutes } from './routes/invitation';
-const app = new Hono()
 
-app.use(logger())
-const apiRoutes = app.basePath('/api').route("/auth", authRoutes).route("/events", eventsRoutes).route("/invitations", invitationRoutes).route("/tickets", ticketRoutes)
 
-export default app
-export type ApiRoutes = typeof apiRoutes
+export function createRouter() {
+    return new OpenAPIHono<AppBindings>({
+      strict: false,
+      defaultHook,
+    });
+  }
+  
+  export default function createApp() {
+    const app = createRouter();
+    app.use(serveEmojiFavicon("📝"));
+    app.use(pinoLogger());
+  
+    app.notFound(notFound);
+    app.onError(onError);
+    return app;
+  }
+
+// const app = new Hono()
+
+// app.use(pinoLogger())
+// const apiRoutes = app.basePath('/api').route("/auth", authRoutes).route("/events", eventsRoutes).route("/invitations", invitationRoutes).route("/tickets", ticketRoutes)
+
+// export default app
+// export type ApiRoutes = typeof apiRoutes
